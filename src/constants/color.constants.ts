c984f5cb325eec3c176fa25d3ab0abb47bd7d621
SqTione <sqtione@icloud.com>
2025-05-14 22:00:26 +0300

    Added colors constants

---
+++ src/constants/color.constants.ts
@@ -0,0 +1,14 @@
+export const COLORS = {
+	// General colors
+	white: '#F2F4DF',
+	black: '#000000',
+	primary: '#EB5D35',
+	secondary: '#EB5D35',
+	success: '#018D36',
+	danger: '#E7452E',
+	warning: '#F3B220',
+
+	// Components colors
+	background: '#F2F4DF',
+	text: '#000000'
+}
